14a8ad925d3089ba50883f4cca390b055100171b
SHADRECKMBUNGA <167176720+SHADRECKMBUNGA@users.noreply.github.com>
2024-11-25 18:38:35 +0200

    password recovery

--- farm_wise/lib/components/labor_records/files/home.dart
+++ farm_wise/lib/components/labor_records/files/home.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:farm_wise/components/utils/bottom_nav_bar.dart';
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
 
   @override
   Widget build(BuildContext context) {
